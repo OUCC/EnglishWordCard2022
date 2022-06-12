34d5d7cc3b0832d11ce226bb2872812c1c97d42f
takumb3d <takumiago56620@gmail.com>
2022-06-13 02:37:17 +0900

    カードの調整とフリック削除

--- lib/wordListview.dart
+++ lib/wordListview.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import "model/WordCard.dart";
 import "model/WordStorage.dart";
@@ -13,7 +12,8 @@ class WordList extends StatefulWidget {
 
 class _WordListState extends State<WordList> {
   WordStorage _storage = WebLocalStorage();
-  List<WordCard> _wordList = List<WordCard>.filled(0, new WordCard("",""));
+  List<WordCard> _wordList = List<WordCard>.filled(0, new WordCard("", ""));
+
   @override
   void initState() {
     setState(() {
@@ -25,86 +25,140 @@ class _WordListState extends State<WordList> {
   Widget build(BuildContext context) {
     return MaterialApp(
       // アプリ名
-      title: 'WordList',
-      theme: ThemeData(
-        // テーマカラー
-        primarySwatch: Colors.blue,
-      ),
-      // リスト一覧画面を表示
-      home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'WordList',
-          style: TextStyle(
-            fontSize: 25,
-          ),
+        title: 'WordList',
+        theme: ThemeData(
+          // テーマカラー
+          primarySwatch: Colors.blue,
         ),
-      ),
-      // 一覧画面
-      body: 
-        ListView.builder(
-          itemCount : _wordList.length,
-          itemBuilder :(BuildContext context, int index) {
-            return FlipCard(
-              front: Card(
-                child: Text(
-                  "${_wordList[index].Front}",
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-              ), // カード前面に表示するWidget
-              back: Card  (
-                child: Text(
-                  "${_wordList[index].Back}",
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-              ), // カード背面に表示するWidget
-              direction: FlipDirection.VERTICAL, // カード回転向き(HORIZONTAL:横<デフォルト>, VERTICAL:縦)
-              flipOnTouch: true, // タッチ可否(true:カード部タップで回転する<デフォルト>, false:タップしても回転しない)
-              speed: 500, // 回転スピード(500ミリ秒<デフォルト>)
-              onFlip: () {}, // タップイベント
-              onFlipDone: (isFront) {}, // タップイベント(前面かどうかbool値で判断できる) 
-            );
-          }
-        ),
-        
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
-            heroTag: "btnEdit",
-            child: const Icon(
-              Icons.favorite,
+        // リスト一覧画面を表示
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'WordList',
+              style: TextStyle(
+                fontSize: 25,
+              ),
             ),
-            onPressed: () {},
           ),
-          FloatingActionButton(
-            child: const Icon(
-              Icons.add,
-            ),
-            heroTag: "btnAdd",
-            onPressed: () async {
-              await Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return Wordform(storage:this._storage, isNewWord:true);
-                  },
+          // 一覧画面
+          body: ListView.builder(
+              itemCount: _wordList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Dismissible(
+                    key: ObjectKey('key1'),
+                    onDismissed: (direction) {
+                      setState(() {
+                        _wordList.removeAt(index);
+                      });
+                      // スワイプ方向がendToStart（画面左から右）の場合の処理
+                      if (direction == DismissDirection.endToStart) {
+                        Scaffold.of(context).removeCurrentSnackBar();
+                        Scaffold.of(context)
+                            .showSnackBar(SnackBar(content: Text("削除しました")));
+                        // スワイプ方向がstartToEnd（画面右から左）の場合の処理
+                      } else {
+                        Scaffold.of(context).removeCurrentSnackBar();
+                        Scaffold.of(context)
+                            .showSnackBar(SnackBar(content: Text("削除しました")));
+                      }
+                    },
+                    // スワイプ方向がendToStart（画面左から右）の場合のバックグラウンドの設定
+                    background: Container(
+                      alignment: Alignment.centerLeft,
+                      color: Colors.redAccent[700],
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                          child: Icon(Icons.delete_forever,
+                              color: Colors.white)),
+                    ),
+
+                    // スワイプ方向がstartToEnd（画面右から左）の場合のバックグラウンドの設定
+                    secondaryBackground: Container(color: Colors.blue),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: FlipCard(
+                          front: SizedBox(
+                            height: 70,
+                            width: 210,
+                            child: Card(
+                              elevation: 20,
+                              child: Center(
+                                child: Text(
+                                  "${_wordList[index].Front}",
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // カード前面に表示するWidget
+                          back: Center(
+                            child: SizedBox(
+                              height: 70,
+                              width: 210,
+                              child: Card(
+                                elevation: 20,
+                                child: Center(
+                                  child: Text(
+                                    "${_wordList[index].Back}",
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // カード背面に表示するWidget
+                          direction: FlipDirection.VERTICAL,
+                          // カード回転向き(HORIZONTAL:横<デフォルト>, VERTICAL:縦)
+                          flipOnTouch: true,
+                          // タッチ可否(true:カード部タップで回転する<デフォルト>, false:タップしても回転しない)
+                          speed: 500,
+                          // 回転スピード(500ミリ秒<デフォルト>)
+                          onFlip: () {},
+                          // タップイベント
+                          onFlipDone:
+                              (isFront) {}, // タップイベント(前面かどうかbool値で判断できる)
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+
+          floatingActionButton: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FloatingActionButton(
+                child: const Icon(
+                  Icons.add,
                 ),
-              );
-              setState((){
-                _wordList = _storage.GetWords();
-              });
-            },
+                heroTag: "btnAdd",
+                onPressed: () async {
+                  await Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Wordform(
+                            storage: this._storage, isNewWord: true);
+                      },
+                    ),
+                  );
+                  setState(() {
+                    _wordList = _storage.GetWords();
+                  });
+                },
+              ),
+            ],
           ),
-        ],
-      ),
-        backgroundColor: Colors.white,
-      )
-    );
+          backgroundColor: Colors.white,
+        ));
   }
+
   @override
   void dispose() {
     super.dispose();
